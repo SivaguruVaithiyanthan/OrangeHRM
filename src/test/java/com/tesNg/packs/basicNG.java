4a52b0326240c2287223a7182fcce0d943dfeffa
SivaguruVaithiyanthan <yvsivaguru143@gmail.com>
2025-11-11 06:12:06 +0530

    TestNG Plugins with some Features

---
+++ src/test/java/com/tesNg/packs/basicNG.java
@@ -0,0 +1,15 @@
+package com.tesNg.packs;
+
+import org.testng.annotations.Test;
+
+public class basicNG 
+
+{
+	@Test
+	 public void tst()
+	 {
+		 System.out.println("Basic of TestNG");
+	 }
+	 
+	 
+}
